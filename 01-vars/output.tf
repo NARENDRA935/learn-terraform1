9d75af501751d6b585392a89ceb97361b5d7ef5f
Narendra T <narendratarra935@gmail.com>
2024-01-17 20:02:02 +0530

    template

--- 01-vars/output.tf
+++ 01-vars/output.tf
@@ -9,6 +9,11 @@ output "first_value_list" {
 output "second_value_list" {
   value = var.v2[1]
 }
- output "value_in_map" {
-   value = var.v3["xyz"]
- }
+
+output "value_in_map" {
+  value = var.v3["xyz"]
+}
+
+output "value_in_map1" {
+  value = lookup(var.v3,"abc1",none)
+}
