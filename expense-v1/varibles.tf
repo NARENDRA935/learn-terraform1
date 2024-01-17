a7796ef9f99fe7fc4219ff0668b1c456a9df4875
Narendra T <narendratarra935@gmail.com>
2024-01-17 22:12:27 +0530

    template

---
+++ expense-v1/varibles.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "frontend" {
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0200b43c5a335c1ba"]
+
+tags = {
+    Name = "frontend-dev"
+  }
+}
+
+resource "aws_instance" "backend" {
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0200b43c5a335c1ba"]
+
+  tags = {
+    Name = "backend-dev"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0200b43c5a335c1ba"]
+
+  tags = {
+    Name = "mysql-dev"
+  }
+}
