90e0362757b59932dd04036020c59b14a98d3948
Narendra T <narendratarra935@gmail.com>
2024-01-17 19:43:06 +0530

    template

--- 01-vars/output.tf
+++ 01-vars/output.tf
@@ -1,3 +1,14 @@
 output "v1" {
   value = var.v1
 }
+
+output "first_value_list" {
+  value = var.v2[0]
+}
+
+output "second_value_list" {
+  value = var.v2[1]
+}
+ output "value_in_map" {
+   value = var.v3["xyz"]
+ }
--- 01-vars/variables.tf
+++ 01-vars/variables.tf
@@ -1,3 +1,15 @@
 variable "v1" {
   default = "hello world"
 }
+
+variable "v2" {
+  default = [ "hello",100,true]
+}
+# varible can have different data type is aceepting in list or anywhere
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "hundred"
+  }
+}
