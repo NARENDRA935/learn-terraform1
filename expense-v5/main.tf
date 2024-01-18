aad7c6da93f232c39646b5d3474158490f16d55d
Narendra T <narendratarra935@gmail.com>
2024-01-18 21:05:58 +0530

    template

--- expense-v5/main.tf
+++ expense-v5/main.tf
@@ -1,8 +1,8 @@
 module "app_resource" {
   source = "./module/app-resource"
   for_each = var.component
-  instance_type          = lookup(each.component,"instance_type","t3.micro" )
-  name                   = lookup(each.component,"name",null )
+  instance_type          = lookup(each.value,"instance_type","t3.micro" )
+  name                   = lookup(each.value,"name",null )
   vpc_security_group_ids = "vpc_security_group_ids"
   zone_id                = "zone_id"
 }
