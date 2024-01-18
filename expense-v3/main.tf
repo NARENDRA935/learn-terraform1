2aed73c73e6a866808fbd1c757fd1e55ed371dee
Narendra T <narendratarra935@gmail.com>
2024-01-18 15:46:48 +0530

    template

---
+++ expense-v3/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "component" {
+  count                  = length(var.component)
+  ami                    = data.aws_ami.centos8.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = element(var.component,count.index )
+  }
+}
+
+resource "aws_route53_record" "records" {
+  count = length(var.component)
+  zone_id = var.zone_id
+  name    = "${element(var.component,count.index )}-dev"
+  type    = "A"
+  ttl     = 30
+  records =[element(aws_instance.component.*.private_ip,count.index )]
+}
+
+
