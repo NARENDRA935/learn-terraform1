0b7dc6d9e1d6e7d060528c67f994246382fe76cd
Narendra T <narendratarra935@gmail.com>
2024-01-18 18:27:26 +0530

    template

---
+++ expense-v4/varibles.tf
@@ -0,0 +1,28 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0200b43c5a335c1ba"]
+}
+
+variable "zone_id" {
+  default = "Z02327421CM6HX7FT19J5"
+}
+
+variable "component" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.micro"
+    }
+    backend = {
+      name          = "backend"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.micro"
+    }
+  }
+}
