2ff6940f4e47993f344045e53ee08aa953bc93d2
Narendra T <narendratarra935@gmail.com>
2024-01-18 14:29:30 +0530

    template

--- 04-loops-1/main.tf
+++ 04-loops-1/main.tf
@@ -5,3 +5,14 @@ resource "null_resource" "test" {
 output "test" {
   value = null_resource.test.*.id
 }
+
+resource "aws_instance" "test" {
+  count                  = 3
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0200b43c5a335c1ba"]
+
+  tags = {
+    Name = "test"
+  }
+}
