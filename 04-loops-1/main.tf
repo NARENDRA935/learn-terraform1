5c94616d19840536f44a87a1ff394e1f28109192
Narendra T <narendratarra935@gmail.com>
2024-01-18 14:58:26 +0530

    template

--- 04-loops-1/main.tf
+++ 04-loops-1/main.tf
@@ -6,17 +6,17 @@ output "test" {
   value = null_resource.test.*.id
 }
 
-variable "component" {
+variable "components" {
   default = [frontend, backend, mysql]
 }
 
 resource "aws_instance" "test" {
-  count                  = length(var.component)
+  count                  = length(var.components)
   ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0200b43c5a335c1ba"]
 
   tags = {
-    Name = element(var.component, count.index)
+    Name = element(var.components, count.index)
   }
 }
