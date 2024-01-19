bcb5fb308f9599399f473221b946c85c4e7c343a
Narendra T <narendratarra935@gmail.com>
2024-01-19 13:40:16 +0530

    template

--- expense-v5/module/app-resource/main.tf
+++ expense-v5/module/app-resource/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instances" {
   ami                    = data.aws_ami.centos8.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = [var.vpc_security_group_ids]
 
   tags = {
     Name = var.name
